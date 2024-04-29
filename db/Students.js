fc2c99d866d76c26712452f0374051ff549ceed5
haideptrai <haitruong2037@gmail>
2024-04-29 15:30:59 +0700

    quizz

---
+++ db/Students.js
@@ -0,0 +1,41 @@
+﻿[{
+        "username": "haipro",
+        "password": "123456",
+        "fullname": "Truong The Hai",
+        "email": "haittpd04737@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2002-7-30",
+        "schoolfee": "1500000",
+        "marks": "0"
+    },
+    {
+        "username": "thangpro",
+        "password": "123456",
+        "fullname": "Duong Duc Thang",
+        "email": "thangtd4835@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2002-1-16",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "hieupro",
+        "password": "123456",
+        "fullname": "Nguyen Quang Hieu",
+        "email": "hieunqpd04773@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "2002-8-20",
+        "schoolfee": "2000000",
+        "marks": "0"
+    },
+    {
+        "username": "hongcuong",
+        "password": "hongcuong",
+        "fullname": "Nguyễn Hồng Cường",
+        "email": "Nguyenhongcuong@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2002-06-20",
+        "schoolfee": "2000000",
+        "marks": "0"
+    }
+]
